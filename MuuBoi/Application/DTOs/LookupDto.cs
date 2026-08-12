6da1020d62dbc929dfe67e78d30437c9de3f83c1
Helena Da Silva <helenas@furb.br>
2026-08-12 19:03:53 -0300

    feat: add new helper for enuns lookups with dto and unify helpers (data converter)

---
+++ MuuBoi/Application/DTOs/LookupDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.IdentityModel.Abstractions;
+using System.Globalization;
+
+namespace MuuBoi.Application.DTOs
+{
+    public class LookupDto
+    {
+        public int Value { get; set; }
+        public string Label { get; set; } = string.Empty;
+    }
+}
