4d592da23c16c4404f3c840a3d784de0d35d2650
Helena Da Silva <helenas@furb.br>
2026-08-22 13:40:57 -0300

    feat: add new breeds to enum

--- MuuBoi/Domain/Enums/AnimalBreed.cs
+++ MuuBoi/Domain/Enums/AnimalBreed.cs
@@ -11,6 +11,24 @@ namespace MuuBoi.Domain.Enums
         Jersey = 2,
 
         [Description("Híbrida/Mestiça")]
-        Crossbred = 3
+        Crossbred = 3,
+
+        [Description("Pardo Suíço")]
+        BrownSwiss = 4,
+
+        [Description("Simental")]
+        Simmental = 5,
+
+        [Description("Gir Leiteiro")]
+        DairyGir = 6,
+
+        [Description("Girolando")]
+        Girolando = 7,
+
+        [Description("Guzerá Leiteiro")]
+        DairyGuzerat = 8,
+
+        [Description("Sindi")]
+        Sindhi = 9
     }
 }
